140156046157dd400e7953538bba128be5dac4ea
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2020-11-28 15:56:29 +0800

    1

--- ads.js
+++
@@ -1,1 +0,0 @@
-document.write('<script src="http://wm.lrswl.com/page/s.php?s=314636&w=468&h=60"></script>');
---
+++ adsyy.js
@@ -0,0 +1,13 @@
+document.write('<scr'+'ipt src="//new.xiongzhangad.com/cf.aspx?action=cycadget&ad_class=7&userid=4233&lowunionusername=&clickstate=1&adshowtype=AdCode_sjdb&ad_size=640x100&newadsel=1&maxadid=&prohibit="></scr'+'ipt>');
+(function (){
+                        var id = "2415689770463175004-10236";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.fzdeng.com/o.js"></script>');
+})();
+(function (){
+                        var id = "2415689770463175004-7548";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.fzdeng.com/o.js"></script>');
+})();
