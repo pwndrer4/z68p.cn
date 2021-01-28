9bab3066208a103a1301611db486dcccfb73fe2f
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2021-01-28 17:27:17 +0800

    Update adsyy.js

--- adsyy.js
+++ adsyy.js
@@ -10,4 +10,3 @@
   (window.adbyunion = window.adbyunion || []).push(id);
   document.write('<script async defer src="https://www.ranqibio.com/o.js"></script>');
 })();
-document.write('<scr'+'ipt src="//www.1888pro.com/c.aspx?action=c&c1=7&c2=39&c3=&c4=2&c5=AdCode_sjdb&c6=640x150&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
