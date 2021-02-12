ecad8132f8f2b323e9769711544ea5c0be7b1de5
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2021-02-12 16:36:56 +0800

    Update adsyy.js

--- adsyy.js
+++ adsyy.js
@@ -2,11 +2,11 @@
                         var id = "2415689770463175004-10236";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.ranqibio.com/o.js"></script>');
+  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
 })();
 (function (){
                         var id = "2415689770463175004-7548";
   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
   (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.ranqibio.com/o.js"></script>');
+  document.write('<script async defer src="https://www.happyyong.com/o.js"></script>');
 })();
